7c7d82c7dfe86f78c2c41b9da4609d9a39774e08
devigorlara <dev.igorlara@gmail.com>
2023-04-17 20:50:43 -0300

    created the class model

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,3 +1,4 @@
+import 'package:app_compras/controllers/popular_product_controller.dart';
 import 'package:app_compras/data/api/api_client.dart';
 import 'package:app_compras/data/repository/popular_product_repo.dart';
 
@@ -5,5 +6,8 @@ import 'package:get/get.dart';
 
 Future<void> init() async {
   Get.lazyPut(() => ApiClient(appBaseUrl: "appBaseUrl"));
+
   Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+
+  Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:app_compras/view/food/recommended_food_detail.dart';
-import 'package:app_compras/view/home_view.dart';
-import 'package:app_compras/view/food/popular_food_details_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:app_compras/helper/dependencies.dart' as dep;
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(),
